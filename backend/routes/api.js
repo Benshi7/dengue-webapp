36024293524935fbd6e01faed088096f6fee1935
Bruno <kurwee@gmail.com>
2024-10-25 19:14:29 -0300

    Dengue SQL

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,8 +1,29 @@
 const express = require('express')
+const mysql = require('mysql2')
 const router = express.Router()
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'dengue_database'
+})
 
 router.get('/', (req, res) => {
   res.send('Saludando desde la api 😌')
+})
+
+router.get('/dengue', (req, res) => {
+  const query = 'SELECT * FROM dengue_data WHERE id=5'
+
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error('Error querying the database:', err)
+      return res.status(500).send('Error querying the database')
+    }
+
+    //
+    res.json(results)
+  })
 })
 
 module.exports = router
