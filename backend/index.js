6b7e80aafcc073f157950682af84f15d009cb508
LeilaGiselleDeLuca <lgdl88@gmail.com>
2024-11-03 18:22:19 -0300

    "Post, Put y Delete - Se incorpora Swagger"

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,13 @@ const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
 
+
 const app = express()
+
+const swaggerUIPath= require("swagger-ui-express");
+const swaggerjsonFilePath = require("./swagger.json");
+app.use("/api-docs", swaggerUIPath.serve, swaggerUIPath.setup(swaggerjsonFilePath));
+
 app.use(cors())
 app.use(helmet())
 app.use(express.json())
@@ -10,7 +16,7 @@ app.use(express.json())
 const PORT = process.env.PORT || 5000
 
 const apiRoutes = require('./routes/api')
-app.use('/api', apiRoutes)
+app.use('/', apiRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running in ${PORT}`)
