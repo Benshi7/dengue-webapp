ae8ea5328d27ebb7be3aaee680f1c701eddb3556
Mtreleani <88211502+Mtreleani@users.noreply.github.com>
2024-11-04 21:52:16 -0300

    Update filtros.jsx

--- frontend/src/components/Dashboard/filtros.jsx
+++ frontend/src/components/Dashboard/filtros.jsx
@@ -31,9 +31,29 @@ const Filtros = ({ setFiltros }) => {
                 >
                     <option value="">Todas</option>
                     <option value="Buenos Aires">Buenos Aires</option>
+                    <option value="CABA">CABA</option>
+                    <option value="Catamarca">Catamarca</option>
+                    <option value="Chaco">Chaco</option>
+                    <option value="Chubut">Chubut</option>
                     <option value="Córdoba">Córdoba</option>
+                    <option value="Corrientes">Corrientes</option>
+                    <option value="Entre Ríos">Entre Ríos</option>
+                    <option value="Formosa">Formosa</option>
+                    <option value="Jujuy">Jujuy</option>
+                    <option value="La Pampa">La Pampa</option>
+                    <option value="La Rioja">La Rioja</option>
+                    <option value="Mendoza">Mendoza</option>
+                    <option value="Misiones">Misiones</option>
+                    <option value="Neuquén">Neuquén</option>
+                    <option value="Río Negro">Río Negro</option>
+                    <option value="Salta">Salta</option>
+                    <option value="San Juan">San Juan</option>
+                    <option value="San Luis">San Luis</option>
+                    <option value="Santa Cruz">Santa Cruz</option>
                     <option value="Santa Fe">Santa Fe</option>
-                    {/* Agregar más opciones de provincia según sea necesario */}
+                    <option value="Santiago del Estero">Santiago del Estero</option>
+                    <option value="Tierra del Fuego">Tierra del Fuego</option>
+                    <option value="Tucumán">Tucumán</option>
                 </select>
             </div>
 
@@ -51,10 +71,17 @@ const Filtros = ({ setFiltros }) => {
                     }}
                 >
                     <option value="">Todos</option>
-                    <option value="0-10">0-10 años</option>
-                    <option value="11-20">11-20 años</option>
-                    <option value="21-30">21-30 años</option>
-                    {/* Agregar más rangos etarios según sea necesario */}
+                    <option value="13-24">13-24 meses</option>
+                    <option value="2-4">2-4 años</option>
+                    <option value="5-9">5-9 años</option>
+                    <option value="5-9">5-9 años</option>
+                    <option value="10-14">10-14 años</option>
+                    <option value="15-19">15-19 años</option>
+                    <option value="20-24">20-24 años</option>
+                    <option value="25-34">25-34 años</option>
+                    <option value="35-44">35-44 años</option>
+                    <option value="45-65">45-65 años</option>
+                    <option value="Mayores 65">65 años o más</option>
                 </select>
             </div>
 
