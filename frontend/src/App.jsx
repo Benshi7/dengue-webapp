5e4dfebdf775be999dadc8966fc560efd1a350f1
Mtreleani <88211502+Mtreleani@users.noreply.github.com>
2024-11-04 21:37:28 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
 import Home from './components/Home/Home'
+import DashboardPage from './components/Dashboard/DashboardPage';
 
 function App () {
   return (
     <>
       <Home />
+      <div className="App">
+        <DashboardPage />
+      </div>
     </>
+    
   )
 }
 
